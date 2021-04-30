640b8b68b1ae26cb72a0ed622d86cf48936a4ba5
nonanonno <as.nonanonno@gmail.com>
2021-04-30 21:17:42 +0000

    test: test install directive
    This checks if any files and directory can be installed or be created.

--- colcon-dub/test/test_dub.py
+++ colcon-dub/test/test_dub.py
@@ -26,13 +26,15 @@ def _search_workspace(upper_than=2) -> Optional[str]:
 
 WORKSPACE_ROOT = _search_workspace()
 PATH_TO_THIS = str(Path(__file__).absolute().parent)
+PKG = 'dub_test_package'
+INSTALL = f'install/{PKG}'
 
 
 def test_dub_package_execute(bash: pytest_shell.shell.bash):
     """Check if the dub package was built by executing."""
     bash.cd(WORKSPACE_ROOT)
     assert bash.run_script_inline([
-        './install/dub_test_package/lib/dub/dub_test_package/dub_test_package'
+        f'./{INSTALL}/lib/dub/{PKG}/{PKG}'
     ]) == 'Hello, World!'
 
 
@@ -54,3 +56,29 @@ def test_colcon_test_fail(bash: pytest_shell.shell.bash):
         f'colcon test --paths {PATH_TO_THIS}/* --dub-args -i fail'
     ])
     assert bash.last_return_code != 0
+
+
+def test_install_files():
+    """Check if the dub package can install any files."""
+    file_list = [
+        f'share/{PKG}/test_install/aaa.txt',
+        f'share/{PKG}/test_install/{PKG}/ccc.txt'
+    ]
+    for f in file_list:
+        assert (Path(WORKSPACE_ROOT) / INSTALL / f).is_file()
+    dir_list = [
+        f'share/{PKG}/test_install/bbb'
+    ]
+    for d in dir_list:
+        assert (Path(WORKSPACE_ROOT) / INSTALL / d).is_dir()
+
+
+def test_create_files():
+    """Check if the dub package can create any files."""
+    file_list = [
+        f'share/{PKG}/test_create/aaa',
+        f'share/{PKG}/test_create/bbb',
+        f'share/{PKG}/test_create/second/ccc',
+    ]
+    for f in file_list:
+        assert (Path(WORKSPACE_ROOT) / INSTALL / f).is_file()
