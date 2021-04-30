58a7b2d86398e85da30cfeca2521f7fbd72b078e
nonanonno <as.nonanonno@gmail.com>
2021-04-30 20:47:42 +0000

    feat: ament_prefix_path
    This add envorinment hooks of ament_prefix_path for ament_dub.

--- colcon-ros-dub/colcon_ros_dub/task/ament_dub/build.py
+++ colcon-ros-dub/colcon_ros_dub/task/ament_dub/build.py
@@ -2,10 +2,13 @@
 # Licensed under the Apache License, Version 2.0
 """Implement build task for ROS DUB package."""
 
+from pathlib import Path
+
 from colcon_dub.task.dub.build import DubBuildTask
 from colcon_core.logging import colcon_logger
 from colcon_core.plugin_system import satisfies_version
 from colcon_core.task import TaskExtensionPoint
+from colcon_core.shell import create_environment_hook
 
 logger = colcon_logger.getChild(__name__)
 
@@ -23,10 +26,14 @@ class AmentDubBuildTask(TaskExtensionPoint):
         logger.info("Building ROS package in '{args.path}'"
                     "with build type 'ros.ament_dub'".format_map(locals()))
 
-        dub_build_task = DubBuildTask()
-        dub_build_task.set_context(context=self.context)
+        extension = DubBuildTask()
+        extension.set_context(context=self.context)
 
-        rc = await dub_build_task.build(additional_hooks=additional_hooks,
-                                        skip_hook_creation=skip_hook_creation)
+        additional_hooks = create_environment_hook(
+            'ament_prefix_path', Path(args.install_base),
+            self.context.pkg.name, 'AMENT_PREFIX_PATH', '', mode='prepend')
+
+        rc = await extension.build(additional_hooks=additional_hooks,
+                                   skip_hook_creation=skip_hook_creation)
         if rc:
             return rc
